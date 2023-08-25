e8ee33b238254e69779aab96dcc720d970809f6b
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-25 17:49:11 +0530

    redis added3

--- redis.sh
+++ redis.sh
@@ -42,7 +42,7 @@ yum install redis -y &>>$LOGFILE
 
 VALIDATE $? "installing redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>>$LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf &>>$LOGFILE
 
 VALIDATE $? "updated address"
 
