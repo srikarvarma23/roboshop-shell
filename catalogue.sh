339316dbbf51d9dee3ebe6c66893c27e54398428
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-25 15:26:15 +0530

    catalogue added2

--- catalogue.sh
+++ catalogue.sh
@@ -62,7 +62,7 @@ else
 
 VALIDATE $? "creating directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 
 VALIDATE $? "zip"
 
