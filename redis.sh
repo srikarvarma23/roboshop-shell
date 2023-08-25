47022df8684bc0ba3ec78897d8fab25edd4b896e
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-25 17:11:41 +0530

    catalogue added3

---
+++ redis.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+LOGSDIR=/tmp
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+ echo -e " $R error: please do it with root access $N"
+     exit 1 
+fi 
+
+VALIDATE(){
+ 
+ if [ $1 -ne 0 ]
+ then 
+  echo -e  " $R  $2 ..... failure $N"
+  exit 1
+  else 
+   echo -e " $G  $2 ..... success $N"
+   fi 
+}
+
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOGFILE
+
+VALIDATE $? "install rpms"
+
+yum module enable redis:remi-6.2 -y &>>$LOGFILE
+
+VALIDATE $? "enable redis version"
+
+yum install redis -y &>>$LOGFILE
+
+VALIDATE $? "installing redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>>$LOGFILE
+
+VALIDATE $? "updated address"
+
+systemctl enable redis &>>$LOGFILE
+
+VALIDATE $? "enambling redis"
+
+systemctl start redis &>>$LOGFILE
+
+VALIDATE $? "starting redis"
