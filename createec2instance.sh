f4667cfc2d3df9243116835e5477ebccba87fd12
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-28 14:31:26 +0530

    ec21 added

--- createec2instance.sh
+++ createec2instance.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 NAMES=("mongodb" "redis" "rabbitmq" "mysql" "catalogue" "user" "cart" "shipping" "payments" "dispatch")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP=sg-00e5e92a347f79a10
+
 
 for i in "${NAMES[@]}"
 do 
-  echo "NAMES: $i"
+ if [[ $i == "mongodb" || $i == "mysql" ]]
+ then 
+  INSTANCE_TYPE="t3.medium"
+  else
+  INSTANCE_TYPE="t2.micro"
+  fi
+  echo "creating instance: $i"
+  aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-00e5e92a347f79a10  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"| jq -r '.Instances[0].PrivateIpAddress'
+
 done
