2537ac13060e0b26337c1d66088da531bb503f93
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-28 14:52:42 +0530

    ec21 added

--- createec2instance.sh
+++ createec2instance.sh
@@ -4,6 +4,7 @@ NAMES=("mongodb" "redis" "rabbitmq" "mysql" "catalogue" "user" "cart" "shipping"
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP=sg-00e5e92a347f79a10
+DOMAIN_NAME=join-devops.online
 
 
 for i in "${NAMES[@]}"
@@ -15,7 +16,22 @@ do
   INSTANCE_TYPE="t2.micro"
   fi
   echo "creating instance: $i"
-  IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-00e5e92a347f79a10  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+  IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
   echo "created instance: $IP_ADDRESS"
 
+
+aws route53 change-resource-record-sets --hosted-zone-id Z011074929JEGW8SOZO4S --change-batch '
+{
+                 
+                 "Changes": [{
+                             "Action": "CREATE",
+                            "ResourceRecordSet": {
+                                "Name": "'$i.$DOMAIN_NAME'",
+                                    "Type": "A",
+                                     "TTL": 300,
+                                  "ResourceRecords": [{"Value": "'$IP_ADDRESS'"}]
+                            }}]
+                            }
+                            '
+                            
 done
